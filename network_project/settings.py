6e6637b208b66ee5e2d54b65db075de80b990c03
Tai Jun Jie <tjunjie2000@gmail.com>
2021-01-15 13:45:27 +0800

    test

--- network_project/settings.py
+++ network_project/settings.py
@@ -28,7 +28,7 @@ TEMPLATE_DIR = BASE_DIR.joinpath("templates")
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = env.str('SECRET_KEY')
+SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
