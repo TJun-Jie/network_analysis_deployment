06ecc9c0dc09771b6dc8ba0ded5d9763ba1225aa
Tai Jun Jie <tjunjie2000@gmail.com>
2021-01-15 13:47:55 +0800

    test

--- network_project/settings.py
+++ network_project/settings.py
@@ -9,15 +9,12 @@ https://docs.djangoproject.com/en/3.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.1/ref/settings/
 """
-import environ
 import os
 from pathlib import Path
 import django_heroku
 
 
 
-env = environ.Env()
-environ.Env.read_env()
 
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
