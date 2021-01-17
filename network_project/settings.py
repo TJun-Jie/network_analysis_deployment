18678400b074560540fc22f90ad229a039b5f770
TJun-Jie <tjunjie2000@gmail.com>
2021-01-17 20:12:57 +0800

    added accses key settings

--- network_project/settings.py
+++ network_project/settings.py
@@ -148,6 +148,6 @@ DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 
 if 'AWS_ACCESS_KEY_ID' in os.environ:
     AWS_ACCESS_KEY_ID = os.environ['AWS_ACCESS_KEY_ID']
-    AWS_SECRET_KEY = os.environ['AWS_SECRET_KEY']
+    AWS_SECRET_ACCESS_KEY = os.environ['AWS_SECRET_ACCESS_KEY']
     AWS_STORAGE_BUCKET_NAME = os.environ['AWS_STORAGE_BUCKET_NAME']
     AWS_S3_REGION_NAME = os.environ['AWS_S3_REGION_NAME']
