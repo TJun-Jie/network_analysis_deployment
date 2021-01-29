68ed186f07afaa4783887d1a71b572f7f9105210
Evande <crossjab@gmail.com>
2021-01-29 12:28:40 +0800

    'password_reset'

--- populate_main_app.py
+++ populate_main_app.py
@@ -18,10 +18,12 @@ def add_student(N=10):
         fake_firstname = fakegen.first_name()
         fake_lastname = fakegen.last_name()
         fake_username = fake_firstname[:2]+fake_lastname[:-2]
-        fake_pass= 'password'
+        fake_pass= 'Junjie2000'
 
-        users = User.objects.get_or_create(username=fake_username,first_name=fake_firstname,last_name=fake_lastname,password=fake_pass)[0]
-
+        users = User.objects.get_or_create(username=fake_username,first_name=fake_firstname,last_name=fake_lastname)[0]
+        u=User.objects.get(username=fake_username)
+        u=set_password(fake_pass)
+        u.save()
         
 # seed to add 2 friends for each student while selecting a popular kid
 # every student will choose the popular student as their friend (except the popular student himself)
@@ -97,11 +99,18 @@ def delete_all_friendships():
 def show_friend_with_id(id):
     print(Friendship1.objects.filter(student__id = id))
 
+#reset password for all students
+def reset_all_password():
+    all_students = User.objects.filter(is_staff=False)
+    for entry in all_students:
+        entry.set_password('Junjie2000')
+        entry.save()
 if __name__ =='__main__':
     print('populating script!')
     # add_student(40)
     # show_students()
-    add_friendship()
+    # add_friendship()
     # delete_all_friendships()
     # show_friend_with_id(30)
+    # reset_all_password()
     print("Populating complete!")
